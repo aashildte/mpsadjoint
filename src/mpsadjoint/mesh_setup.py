28aabddbba3996143e502b0018fbfa3cb48999e9
Henrik Finsberg <henrikfinsberg@hotmail.com>
2023-12-19 09:22:43 +0100

    Fix mypy issues

--- src/mpsadjoint/mesh_setup.py
+++ src/mpsadjoint/mesh_setup.py
@@ -7,16 +7,19 @@ Loads mesh + bc surfaces for pillars
 """
 
 import os
+import typing
 import dolfin as df
 import dolfin_adjoint as da
 from mpi4py import MPI
 from pathlib import Path
-from collections import namedtuple
-
-Geometry = namedtuple("Geometry", ["mesh", "ds"])
 
 
-def load_mesh_h5(filename: os.PathLike, save_pvd_file: bool = False) -> Geometry:
+class Geometry(typing.NamedTuple):
+    mesh: df.Mesh
+    ds: df.Measure
+
+
+def load_mesh_h5(filename: typing.Union[str, os.PathLike], save_pvd_file: bool = False) -> Geometry:
     comm = MPI.COMM_WORLD
     filename = Path(filename)
     if not filename.is_file():

45e02b4f2db6e0c9a25e558138bef2b102a57fbf
Åshild Telle <aashildte@gmail.com>
2023-12-20 16:45:46 -0800

    - inverse_cp.py from init file (which was just a scp of inverse.py

--- src/mpsadjoint/__init__.py
+++ src/mpsadjoint/__init__.py
@@ -1,5 +1,4 @@
 from . import cardiac_mechanics
-from . import inverse_cp
 from . import inverse
 from . import io_files
 from . import mesh_setup
@@ -9,7 +8,6 @@ from . import nonlinearproblem
 
 __all__ = [
     "cardiac_mechanics",
-    "inverse_cp",
     "inverse",
     "io_files",
     "mesh_setup",
